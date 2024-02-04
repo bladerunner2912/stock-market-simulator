d9eccb5ab4b76d3d7bd22d450217065001850f3e
bladerunner2912 <bladerunner2912@gmail.com>
2024-02-04 19:47:34 +0530

    Project init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,61 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import {
+  initializeAppCheck,
+  ReCaptchaEnterpriseProvider,
+  provideAppCheck,
+} from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import {
+  getRemoteConfig,
+  provideRemoteConfig,
+} from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'stock-market-simulator-20a14',
+          appId: '1:349731210657:web:5177358d4455666f3e37e0',
+          databaseURL:
+            'https://stock-market-simulator-20a14-default-rtdb.asia-southeast1.firebasedatabase.app',
+          storageBucket: 'stock-market-simulator-20a14.appspot.com',
+          apiKey: 'AIzaSyD7dpUtAjN9s3WH1xn6rDqQxY1hJsh9oCo',
+          authDomain: 'stock-market-simulator-20a14.firebaseapp.com',
+          messagingSenderId: '349731210657',
+          measurementId: 'G-EDKJ9KPPLS',
+        }),
+      ),
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    ScreenTrackingService,
+    UserTrackingService,
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideFunctions(() => getFunctions())),
+    importProvidersFrom(provideMessaging(() => getMessaging())),
+    importProvidersFrom(providePerformance(() => getPerformance())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+    importProvidersFrom(provideRemoteConfig(() => getRemoteConfig())),
+  ],
 };
---
+++ src/app/enviornments/firebaseconfig.js
@@ -0,0 +1,23 @@
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7dpUtAjN9s3WH1xn6rDqQxY1hJsh9oCo",
+
+  authDomain: "stock-market-simulator-20a14.firebaseapp.com",
+
+  projectId: "stock-market-simulator-20a14",
+
+  storageBucket: "stock-market-simulator-20a14.appspot.com",
+
+  messagingSenderId: "349731210657",
+
+  appId: "1:349731210657:web:5177358d4455666f3e37e0",
+
+  measurementId: "G-EDKJ9KPPLS",
+};
